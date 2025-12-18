3ab3af4234200ece2e541d681919a3c62f671140
Sicheng Mu <musicheng520@gmail.com>
2025-12-18 20:00:02 +0000

    getMovies now using our Movies API

--- react-movies/src/api/tmdb-api.jsx
+++ react-movies/src/api/tmdb-api.jsx
@@ -1,3 +1,4 @@
+/*
 export const getMovies = ({ queryKey }) => {
   const [, { category, page }] = queryKey;
 
@@ -20,6 +21,22 @@ export const getMovies = ({ queryKey }) => {
     .catch((error) => {
       throw error;
     });
+};
+*/
+export const getMovies = () => {
+  return fetch(
+    `http://localhost:8080/api/movies/discover`
+  ).then((response) => {
+    if (!response.ok) {
+      return response.json().then((error) => {
+        throw new Error(error.status_message || "Something went wrong");
+      });
+    }
+    return response.json();
+  })
+  .catch((error) => {
+      throw error
+  });
 };
 
 
